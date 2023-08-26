cb438eb7246eb12b041dadbf0e74578d8d1d43a4
shamad <fuzzriff@gmail.com>
2023-08-26 15:01:29 +0300

    first commit

---
+++ 12-promise-race/index.js
@@ -0,0 +1,26 @@
+function race(promises) {
+  const res = new Promise((resolve, reject) => {
+    promises.forEach((promise) => promise.then(resolve).catch(reject));
+  });
+  return res;
+}
+
+const testPromise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "first promise");
+});
+
+const testPromise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, "second promise");
+});
+
+const testPromise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, "third promise");
+});
+
+race([testPromise1, testPromise2, testPromise3])
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
+
+Promise.race([testPromise1, testPromise2, testPromise3])
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
