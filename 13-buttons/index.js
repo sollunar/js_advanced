e80d6932c2d957a58706858ff11acad19561cd45
shamad <fuzzriff@gmail.com>
2023-08-27 22:35:51 +0300

    initial commit

---
+++ 13-buttons/index.js
@@ -0,0 +1,29 @@
+"use-strict";
+
+const counter = document.querySelector(".click_counter");
+const buttonsContainer = document.querySelector(".buttons");
+let prevButton;
+
+for (let i = 0; i < 5; i++) {
+    const button = document.createElement("button");
+    button.classList.add("button");
+    button.innerHTML = "Нажми меня!";
+    buttonsContainer.append(button);
+}
+
+buttonsContainer.addEventListener("click", function(e) {
+    const currentButton = e.target;
+
+    if (currentButton.className !== "button") {
+        return;
+    }
+
+    counter.innerHTML = Number(counter.innerHTML) + 1;
+    currentButton.innerHTML = "Нажата!";
+
+    if (prevButton && prevButton !== currentButton) {
+        prevButton.innerHTML = "Нажми меня!";
+    }
+
+    prevButton = currentButton;
+});
