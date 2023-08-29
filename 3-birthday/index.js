1d24d332417a0b9e5c0a7785ba20ac9939e4221b
shamad <fuzzriff@gmail.com>
2023-08-29 23:59:08 +0300

    code overengineering

--- 3-birthday/index.js
+++ 3-birthday/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const validBirthday = "2000-08-20";
-const invalidBirthday = "2009-08-31";
+const invalidBirthday = "2009-08-30";
 
-function isOverFourteen(dateString) {
+function validateAge(dateString, ageRestriction) {
   const birthday = new Date(dateString);
   const today = new Date();
 
   const fourteenYears = new Date(
-    today.getFullYear() - 14,
+    today.getFullYear() - ageRestriction,
     today.getMonth(),
     today.getDate(),
     today.getHours(),
@@ -18,5 +18,5 @@ function isOverFourteen(dateString) {
   return birthday.getTime() <= fourteenYears.getTime();
 }
 
-console.log(isOverFourteen(validBirthday));
-console.log(isOverFourteen(invalidBirthday));
+console.log(validateAge(validBirthday, 14));
+console.log(validateAge(invalidBirthday, 14));
