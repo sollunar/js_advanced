8e2334225676e0e9dedfa59301d3e9efeaf8b148
shamad <fuzzriff@gmail.com>
2023-08-29 16:37:12 +0300

    reviewed changes

--- 2-dice/index.js
+++ 2-dice/index.js
@@ -1,4 +1,6 @@
-const diceTypes = {
+"use-strict";
+
+const DICE_TYPES = {
   d4: 4,
   d6: 6,
   d8: 8,
@@ -6,16 +8,29 @@ const diceTypes = {
   d12: 12,
   d16: 16,
   d20: 20,
+  d21: "dsfsd",
 };
 
 function throwDice(diceType) {
-  if (isNaN(parseInt(diceType))) {
+  const dice = DICE_TYPES[diceType];
+
+  if (!dice) {
+    throw new Error(
+      `ArgumentError: Invalid dice type\nAvaliable dice types: ${Object.keys(
+        DICE_TYPES
+      ).join(",")}`
+    );
+  }
+
+  if (isNaN(parseInt(dice))) {
     throw new Error(
       `ArgumentError: Function only accepts numbers\nReceived: ${typeof diceType}`
     );
   }
-  return Math.floor(Math.random() * diceType + 1);
+
+  return Math.floor(Math.random() * dice + 1);
 }
 
-console.log(throwDice(diceTypes.d20));
-console.log(throwDice("foo-bla"));
+console.log(throwDice("d20"));
+console.log(throwDice("bla-blah"));
+console.log(throwDice(100));
