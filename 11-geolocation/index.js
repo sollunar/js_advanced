db257526e5630ffe7b675b5c8b52c79d1c5f5c84
shamad <fuzzriff@gmail.com>
2023-08-29 20:33:15 +0300

    safe check for navigator obj

--- 11-geolocation/index.js
+++ 11-geolocation/index.js
@@ -1,11 +1,15 @@
 function getUserCoordinates() {
   return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      resolve({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
       });
-    });
+    } else {
+      reject(new Error("Geolocation is not supported by this browser"));
+    }
   });
 }
 
