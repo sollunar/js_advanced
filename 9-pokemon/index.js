ca3e034f5595e60d1ec59cb4b49beadb0da75b3f
shamad <fuzzriff@gmail.com>
2023-08-29 20:12:13 +0300

    reviewed changes

--- 9-pokemon/index.js
+++ 9-pokemon/index.js
@@ -2,21 +2,26 @@
 
 const API = "https://pokeapi.co/api/v2/pokemon";
 
-const request = new XMLHttpRequest();
-
-request.open("GET", `${API}/ditto`);
-request.send();
-
-request.addEventListener("load", function () {
-  const { abilities } = JSON.parse(this.responseText);
+function getPokemon(pokemonName) {
   const request = new XMLHttpRequest();
-  request.open("GET", abilities[0].ability.url);
+  request.open("GET", `${API}/${pokemonName}`);
   request.send();
+
   request.addEventListener("load", function () {
-    const response = JSON.parse(this.responseText);
-    const result = response.effect_entries.find(
-      (entry) => entry.language.name === "en"
-    );
-    console.log(result);
+    const { abilities } = JSON.parse(this.responseText);
+    const request = new XMLHttpRequest();
+    request.open("GET", abilities[0].ability.url);
+    request.send();
+    request.addEventListener("load", function () {
+      const response = JSON.parse(this.responseText);
+      const result = response.effect_entries.find(
+        (entry) => entry.language.name === "en"
+      );
+      console.log(result);
+    });
   });
-});
+}
+
+getPokemon("ditto");
+getPokemon("pikachu");
+getPokemon("charizard");
